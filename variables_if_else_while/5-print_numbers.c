20f69564247b5e3889b1d374633bbaa74d66421a
user76335 <11958@holbertonstudents.com>
2025-08-28 14:14:38 +0000

    h

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints numbers from 0 to 9 using putchar
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char c;
-c = '0';
-while (c <= '9')
-{
-putchar(c);
-c++;
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
 }
-putchar('\n');
-return (0);
-}
+
