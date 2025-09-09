720d6a52d18485e5344e5073c361804ea6333a0f
user76335 <11958@holbertonstudents.com>
2025-09-09 15:11:35 +0000

    G

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,12 @@ int len = 0;
 int i, start;
 
 while (str[len] != '\0')
-        len++;
+len++;
 
 if (len % 2 == 0)
-        start = len / 2;
+start = len / 2;
 else
-        start = (len + 1) / 2;
+start = (len + 1) / 2;
 
 for (i = start; i < len; i++)
         _putchar(str[i]);
