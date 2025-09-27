b15bff1742e8f7411c1c7f4acb7707d0465d463e
user76335 <11958@holbertonstudents.com>
2025-09-27 22:45:02 +0000

    G

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,18 +2,24 @@
 #define DOG_H
 
 /**
- * struct dog - structure for a dog
- * @name: name of the dog (string)
- * @age: age of the dog (float)
- * @owner: owner of the dog (string)
- */
+* struct dog - structure for a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 typedef struct dog dog_t;
 
-#endif /* DOG_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner); /* <-- Add this */
+void free_dog(dog_t *d);
+
+#endif
+
